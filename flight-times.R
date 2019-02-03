b04fcc819c587c9220fed517ca984bb891365b12
p0bs <robin_penfold@yahoo.com>
2019-02-03 17:51:37 +0000

    Adding LGW and pruning results. Dedupe later?

--- flight-times.R
+++ flight-times.R
@@ -1,22 +1,41 @@
 library(airportr)
+library(leaflet)
 library(rvest)
 library(tidyverse)
 library(xml2)
  
-tags <- c('.airport-content-destination-list-time', '.airport-content-destination-list-name')
-index_page <- 'https://www.flightsfrom.com/LHR/destinations'
+index_page_LHR <- 'https://www.flightsfrom.com/LHR/destinations'
+index_page_LGW <- 'https://www.flightsfrom.com/LGW/destinations'
 
-airports <- read_html(index_page) %>%
+airports_LHR <- read_html(index_page_LHR) %>%
   html_nodes(".airport-content-destination-list-name") %>% 
   html_text() %>% 
   str_trim()
 
-times <- read_html(index_page) %>%
+times_LHR <- read_html(index_page_LHR) %>%
   html_nodes(".airport-content-destination-list-time") %>% 
   html_text() %>% 
   str_trim()
 
-data <- tibble(airports, times) %>% 
+airports_LGW <- read_html(index_page_LGW) %>%
+  html_nodes(".airport-content-destination-list-name") %>% 
+  html_text() %>% 
+  str_trim()
+
+times_LGW <- read_html(index_page_LGW) %>%
+  html_nodes(".airport-content-destination-list-time") %>% 
+  html_text() %>% 
+  str_trim()
+
+data <- bind_rows(
+  tibble(airports = airports_LHR, times = times_LHR, home = 'LHR'),
+  tibble(airports = airports_LGW, times = times_LGW, home = 'LGW')
+) %>% 
+  arrange(home) %>% 
+  group_by(airports) %>% 
+  slice(1) %>% 
+  ungroup() %>% 
+  arrange(airports) %>% 
   rowwise() %>% 
   mutate(
     code = str_extract(airports, '[[:upper:]][[:upper:]][[:upper:]]'),
@@ -34,13 +53,16 @@ data <- tibble(airports, times) %>%
     city = str_trim(str_to_lower(str_sub(string = airports, start = 1L, end = position1))),
     country = str_trim(str_to_lower(str_sub(string = airports, start = position2, end = -1L))))
 
-airports <- airportr::airports
+airport_data <- airportr::airports
 
-output <- data[, c('code', 'city', 'country', 'minutes')] %>% 
-  left_join(airports, by = c('code' = 'IATA')) %>% 
+output <- data[, c('code', 'city', 'home', 'country', 'minutes')] %>% 
+  left_join(airport_data, by = c('code' = 'IATA')) %>% 
   filter(!is.na(ICAO)) %>% 
   rowwise() %>% 
-  mutate(distance = airport_distance('LHR', code))
+  mutate(
+    distance = airport_distance(home, code),
+    City = str_to_lower(City)
+    )
 
 temp_data <- read_html('https://en.m.wikipedia.org/wiki/List_of_cities_by_average_temperature') %>%
   html_nodes("table") %>% 
@@ -48,7 +70,19 @@ temp_data <- read_html('https://en.m.wikipedia.org/wiki/List_of_cities_by_averag
   bind_rows() %>% 
   select(-Year, -Ref.) %>% 
   mutate_at(vars(-Country, -City), str_remove, '\\s*\\([^\\)]+\\)') %>% 
-  mutate_at(vars(-Country, -City), as.numeric)
+  mutate_at(vars(-Country, -City), as.numeric) %>% 
+  mutate(
+    City = str_to_lower(City),
+    Country = str_to_lower(Country)) %>% 
+  arrange(City)
 
-
+overall <- output %>% 
+  left_join(temp_data, by = c('City' = 'City')) %>% 
+  select(code, City, Country.x, minutes, ICAO, Latitude, Longitude, UTC, distance, Aug) %>% 
+  filter(!is.na(Aug)) %>% 
+  arrange(code) %>% 
+  group_by(Country.x, City) %>% 
+  slice(1) %>% 
+  ungroup() %>% 
+  arrange(City)
 
